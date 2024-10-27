5512856da86951a62604e6e68ba74fe114bd32cb
NoobProgramer6918 <bartek.szklarski@outlook.com>
2024-10-27 15:12:10 +0100

    added the class "WinMatrix"

--- src/WinMatrix.cpp
+++ src/WinMatrix.cpp
@@ -3,7 +3,22 @@
 #include <windows.h>
 #include <random>
 
-int main() {
-  
-}
+std::random_device rd;  // Seed for the random number engine
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> distr(0, 9);
 
+class WinMatrix {
+  public:
+  const int Max_trailLEN = 6; // maximum trail lenght
+
+  const char* Trail_char1 = "@";
+  const char* Trail_char2 = "#";
+  const char* Trail_char3 = "&";
+  const char* Trail_char4 = "$";
+  const char* Trail_char5 = ";";
+  const char* Trail_char6 = "-";
+};
+
+int main() {
+  WinMatrix matrix;
+}
