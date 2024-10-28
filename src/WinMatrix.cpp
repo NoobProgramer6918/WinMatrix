5bf2e2baf85828f421f3af983627cd9ed2cfceab
NoobProgramer6918 <bartek.szklarski@outlook.com>
2024-10-28 16:14:10 +0100

    replaced the old func

--- src/WinMatrix.cpp
+++ src/WinMatrix.cpp
@@ -19,6 +19,27 @@ class WinMatrix {
   const char* Trail_char6 = "-";
 };
 
+void get_consoleSize(int &width, int &height);
+
 int main() {
+<<<<<<< Updated upstream:src/WinMatrix.cpp
   WinMatrix matrix;
 }
+=======
+  W_Matrix Wmatrix;
+
+  
+}
+
+void get_consoleSize(int &width, int &height) {
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+        width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    } else {
+        // If we fail to get the size, we can set a default or handle the error.
+        width = 20;
+        height = 15;
+        // width and height is measured in characters not in pixels
+    }
+>>>>>>> Stashed changes:src/WindowsMatrix.cpp
